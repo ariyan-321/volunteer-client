6e251985337dd3bcd1a0e9ba17da8485657df4ae
Your Name <ariyaniphone14@gmail.com>
2025-02-11 06:32:57 +0600

    fixed the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -55,7 +55,7 @@ export default function Navbar() {
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5 lg:hidden"
+                className="h-5 w-5 lg:block"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
